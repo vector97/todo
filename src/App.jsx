094c4b2db4c364c39ef6af795270db157181dac7
Viktor Bogatenkov <vector_97@mail.ru>
2023-12-04 21:10:04 +0400

    feat: изменил корневой компонент на классовый, перенёс задачи в состояние

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,49 @@
+import { Component } from "react";
+
 import NewTaskForm from "./components/NewTaskForm";
 import TaskList from "./components/TaskList";
 import Footer from "./components/Footer";
 
 import "./App.css";
 
-function App() {
-  const todosData = [
-    {
-      id: 1,
-      title: "Completed task",
-      date: new Date(),
-      done: true,
-    },
-    {
-      id: 2,
-      title: "Editing task",
-      date: new Date(),
-      done: false,
-    },
-    {
-      id: 3,
-      title: "Active task",
-      date: new Date(),
-      done: false,
-    },
-  ];
-  console.log(todosData);
+class App extends Component {
+  state = {
+    todosData: [
+      {
+        id: 1,
+        title: "Completed task",
+        date: new Date(),
+        done: true,
+      },
+      {
+        id: 2,
+        title: "Editing task",
+        date: new Date(),
+        done: false,
+      },
+      {
+        id: 3,
+        title: "Active task",
+        date: new Date(),
+        done: false,
+      },
+    ],
+  };
 
-  return (
-    <section className="todoapp">
-      <header className="header">
-        <h1>todos</h1>
-        <NewTaskForm />
-      </header>
-      <section className="main">
-        <TaskList todos={todosData} />
-        <Footer />
+  render() {
+    return (
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <NewTaskForm />
+        </header>
+        <section className="main">
+          <TaskList todos={this.state.todosData} />
+          <Footer />
+        </section>
       </section>
-    </section>
-  );
+    );
+  }
 }
 
 export default App;
